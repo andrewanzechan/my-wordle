a63445d1e06d367f450b620e9d56830aa1ace9d7
Andrew Chan <chan.anze@gmail.com>
2025-04-19 15:30:08 -0400

    Updates

--- client/assets/with-props-CwHmG_V2.js
+++
@@ -1,1 +0,0 @@
-import{v as p,w as t,x as e,y as o,r as s,z as u}from"./chunk-HA7DTUK3-BaDD2dak.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
